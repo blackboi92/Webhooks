c0a1253c4029611714fd748f10f4797050f51d47
blackboi92 <42197468+blackboi92@users.noreply.github.com>
2018-08-08 01:16:34 -0400

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -7,13 +7,13 @@ var options = {
   url: "WEBHOOK_URL",
   method:  "POST",
   headers: headers,
-  form: {"content": "@everyone oliver was here",
-         "username": "Akeno",
+  form: {"content": "@everyone Niggers Are Bad And Should Really Die",
+         "username": "N1GG3RBOIII",
          "avatar_url": "http://i.imgur.com/vYpVvfH.jpg"}
 }
 
 var interval = 10 * 35; // setting interval at .35 seconds so we dont get ratelimited
-for (var x = 0; x < 100; x+=1) { // change '100' to howmany messages u wanna send
+for (var x = 0; x < 100000; x+=1) { // change '100' to howmany messages u wanna send
   setTimeout( function (x) {
     request(options, function(error, body){
       if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
